45d90031a9bfb72d4ff08748f9fec897af6dce9e
Teddy GIBERT <teddygibert@Teddys-MacBook-Air.local>
2024-09-25 14:15:52 +0200

    various iterators

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -14,15 +14,34 @@ public:
 		return *this;
 	}
 
-	typedef	typename std::stack<T>::container_type::iterator	iterator;
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
 	iterator	begin(){
 		return this->c.begin();
 	};
-
 	iterator	end(){
 		return this->c.end();
 	};
-
+	std::stack<T>::container_type::const_iterator	begin() const {
+		return this->c.begin();
+	};
+	std::stack<T>::container_type::const_iterator	end() const {
+		return this->c.end();
+	};
+	std::stack<T>::container_type::reverse_iterator	rbegin(){
+		return this->c.rbegin();
+	};
+	std::stack<T>::container_type::reverse_iterator	rend(){
+		return this->c.rend();
+	};
+	std::stack<T>::container_type::const_reverse_iterator	rbegin() const {
+		return this->c.rbegin();
+	};
+	std::stack<T>::container_type::const_reverse_iterator	rend() const {
+		return this->c.rend();
+	};
 };
 
